c7ba4b47d3c08727db5fee039b91da10bdbcb0e1
NFBug <99752258+NFBug@users.noreply.github.com>
2024-07-13 23:35:47 -0300

    Update adiciona.php

--- adiciona.php
+++ adiciona.php
@@ -1,24 +1,32 @@
-<?php 
+<?php
 require("conecta.php");
 
 $nome = $_POST['nome'];
-$nascimento = $_POST['data'];
+$nascimento = $_POST['nascimento'];
 $renda = $_POST['renda'];
 
 if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+    die("Conexão falhou: " . $conn->connect_error);
 }
 
-$sql = "INSERT INTO aluno (nome,nascimento,renda) VALUES ('$nome', '$nascimento', '$renda')"; 
-
-
-if ($conn->query($sql) == TRUE) {
-    header("location: Cadastro.php");
-}else{
-    echo "Error";
+if (!preg_match('/^[0-9]+([\.|,][0-9]+)?$/', $renda)) {
+    echo "Erro: A renda deve conter apenas valores numéricos válidos.";
+    exit;
 }
 
+$data_formatada = date("Y-m-d", strtotime($nascimento));
+$renda_formatada = str_replace(',', '.', $renda);
+$renda_formatada = floatval($renda_formatada);
 
+$sql = $conn->prepare("INSERT INTO usuarios (nome, renda, nascimento) VALUES (?, ?, ?)");
+$sql->bind_param("sds", $nome, $renda_formatada, $data_formatada);
+
+if ($sql->execute() === TRUE) {
+    header("location: index.php");
+} else {
+    echo "Erro: " . $conn->error;
+}
+
+$sql->close();
 $conn->close();
-
 ?>
