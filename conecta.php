6d32f6ff0a79fc96f6afbbc7608fdfe365c2009c
NFBug <99752258+NFBug@users.noreply.github.com>
2024-07-13 23:36:28 -0300

    Update conecta.php

--- conecta.php
+++ conecta.php
@@ -1,14 +1,12 @@
 <?php
 $servername = "localhost";
 $username = "root";
-$password = "123456";
-$dbname = "PegaTampa";
+$password = "";//senha do seu banco de dados (your database password)
+$dbname = "";//nome do seu banco de dados (your database name)
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 } 
-
-
 ?>
